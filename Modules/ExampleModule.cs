98047a01b19f605105b68d58a05370eb3da6699f
Matthieu <alivecreation.studio@gmail.com>
2019-07-11 22:33:35 +0400

    First commit

---
+++ Modules/ExampleModule.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herobot.Plugins.Example.Modules
+{
+    class ExampleModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("testCommand")]
+        public Task Testcommand() => ReplyAsync("Je suis un test !");
+
+        [Command("mp")]
+        public async Task SendMp(SocketGuildUser socketGuildUser,[Remainder]string message) {
+            await socketGuildUser.GetOrCreateDMChannelAsync().ContinueWith((channelOpen) =>
+            {
+                channelOpen.Result.SendMessageAsync(message);
+            });
+        }
+    }
+}
